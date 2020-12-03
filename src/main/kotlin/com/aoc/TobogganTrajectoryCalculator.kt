849b84e3d6b4fe2dcdb91adea6dc23ded5a2012a
mzottola <melvin.zottola@gmail.com>
2020-12-03 22:29:42 +0100

    Refacto 3

--- src/main/kotlin/com/aoc/TobogganTrajectoryCalculator.kt
+++ src/main/kotlin/com/aoc/TobogganTrajectoryCalculator.kt
@@ -1,6 +1,10 @@
 package com.aoc
 
 class TobogganTrajectoryCalculator(grid: List<String>) {
+    data class AreaMap(val cells: List<CharArray>, val rightLimit: Int) {
+        constructor(cells: List<CharArray>) : this(cells, cells.first().size)
+    }
+
     val areaMap: AreaMap
 
     init {
@@ -8,26 +12,9 @@ class TobogganTrajectoryCalculator(grid: List<String>) {
     }
 
     fun countTrees(slopeX: Int, slopeY: Int): Int {
-        var position = Coordinate(0, 0)
-        val elements = mutableListOf<Char>()
-        while (position.y < areaMap.bottomLimit) {
-            elements.add(getElementAtPosition(position))
-            position = move(position, slopeX, slopeY)
-        }
-        return elements.count { it == '#' }
-    }
-
-    data class AreaMap(val cells: List<CharArray>, val bottomLimit: Int, val rightLimit: Int) {
-        constructor(cells: List<CharArray>) : this(cells, cells.size, cells[0].size)
-    }
-    data class Coordinate(val x: Int, val y: Int)
-
-    private fun move(position: Coordinate, slopeX: Int, slopeY: Int): Coordinate {
-        val x = (position.x + slopeX).let { if (it > areaMap.rightLimit - 1) it - (areaMap.rightLimit) else it }
-        return Coordinate(x, position.y + slopeY)
-    }
-
-    private fun getElementAtPosition(position: Coordinate): Char {
-        return areaMap.cells[position.y][position.x]
+        return areaMap.cells
+            .filterIndexed { index, chars -> index != 0 && index % slopeY == 0 }
+            .mapIndexed { index, chars -> chars[((index + 1) * slopeX) % areaMap.rightLimit] }
+            .count { it == '#' }
     }
 }
